ef9ec144e7b84726be7b306a344ec66ecaf1387e
lidewij-brainlabs <lidewij@brainlabsdigital.com>
2018-09-26 13:59:41 +0100

    Redo Candidate constructor

--- src/Structures/Candidate.php
+++ src/Structures/Candidate.php
@@ -144,43 +144,126 @@ class Candidate
      */
     public $work_history = [];
 
-    /**
-     * @inheritdoc
-     */
     public static function fromResponse(array $rawCandidate)
     {
-        $candidate = new Candidate;
+        $_id = $rawCandidate['_id'];
+        $address = $rawCandidate['address'];
+        $assigned_to = $rawCandidate['assigned_to'];
+        $cover_letter = $rawCandidate['cover_letter'];
+        $creation_date = $rawCandidate['creation_date'];
+        $education = $rawCandidate['education'];
+        $email_address = $rawCandidate['email_address'];
+        $followed_by = $rawCandidate['followed_by'];
+        $headline = $rawCandidate['headline'];
+        $initial = $rawCandidate['initial'];
+        $name = $rawCandidate['name'];
+        $origin = $rawCandidate['origin'];
+        $overall_score = $rawCandidate['overall_score'];
+        $phone_number = $rawCandidate['phone_number'];
+        $profile_photo_url = $rawCandidate['profile_photo_url'];
+        $questionnaire = $rawCandidate['questionnaire'];
+        $recruited_by = $rawCandidate['recruited_by'];
+        $referred_by = $rawCandidate['referred_by'];
+        $sourced_by = $rawCandidate['sourced_by'];
+        $resume = $rawCandidate['resume'];
+        $social_profiles = $rawCandidate['social_profiles'];
+        $source = $rawCandidate['source'];
+        $stage = $rawCandidate['stage'];
+        $summary = $rawCandidate['summary'];
+        $tags = $rawCandidate['tags'];
+        $updated_date = $rawCandidate['updated_date'];
+        $work_history = $rawCandidate['work_history'];
+        $custom_attributes = $rawCandidate['custom_attributes'];
 
-        $candidate->rawData = $rawCandidate;
+        return new Candidate(
+            $_id,
+            $address,
+            $assigned_to,
+            $cover_letter,
+            $creation_date,
+            $education,
+            $email_address,
+            $followed_by,
+            $headline,
+            $initial,
+            $name,
+            $origin,
+            $overall_score,
+            $phone_number,
+            $profile_photo_url,
+            $questionnaire,
+            $recruited_by,
+            $referred_by,
+            $sourced_by,
+            $resume,
+            $social_profiles,
+            $source,
+            $stage,
+            $summary,
+            $tags,
+            $updated_date,
+            $work_history,
+            $custom_attributes
+        );
+    }
 
-        $candidate->id = $rawCandidate['_id'];
-        $candidate->address = $rawCandidate['address'] ?? '';
-        $candidate->assigned_to = $rawCandidate['assigned_to'] ?? [];
-        $candidate->cover_letter = $rawCandidate['cover_letter'] ?? '';
-        $candidate->creation_date = $rawCandidate['creation_date'] ?? '';
-        $candidate->education = $rawCandidate['education'] ?? [];
-        $candidate->email_address = $rawCandidate['email_address'] ?? '';
-        $candidate->followed_by = $rawCandidate['followed_by'] ?? '';
-        $candidate->headline = $rawCandidate['headline'] ?? '';
-        $candidate->initial = $rawCandidate['initial'] ?? '';
-        $candidate->name = $rawCandidate['name'] ?? '';
-        $candidate->origin = $rawCandidate['origin'] ?? '';
-        $candidate->overall_score = $rawCandidate['overall_score'] ?? [];
-        $candidate->phone_number = $rawCandidate['phone_number'] ?? '';
-        $candidate->profile_photo_url = $rawCandidate['profile_photo_url'] ?? '';
-        $candidate->questionnaire = $rawCandidate['questionnaire'] ?? [];
-        $candidate->recruited_by = $rawCandidate['recruited_by'] ?? [];
-        $candidate->referred_by = $rawCandidate['referred_by'] ?? [];
-        $candidate->sourced_by = $rawCandidate['sourced_by'] ?? [];
-        $candidate->resume = $rawCandidate['resume'] ?? [];
-        $candidate->social_profiles = $rawCandidate['social_profiles'] ?? [];
-        $candidate->source = $rawCandidate['source'] ?? [];
-        $candidate->stage = $rawCandidate['stage'] ?? [];
-        $candidate->summary = $rawCandidate['summary'] ?? '';
-        $candidate->tags = $rawCandidate['tags'] ?? [];
-        $candidate->updated_date = $rawCandidate['updated_date'] ?? '';
-        $candidate->work_history = $rawCandidate['work_history'] ?? [];
-
-        return $candidate;
+    public function __construct(
+        $_id,
+        $address,
+        $assigned_to,
+        $cover_letter,
+        $creation_date,
+        $education,
+        $email_address,
+        $followed_by,
+        $headline,
+        $initial,
+        $name,
+        $origin,
+        $overall_score,
+        $phone_number,
+        $profile_photo_url,
+        $questionnaire,
+        $recruited_by,
+        $referred_by,
+        $sourced_by,
+        $resume,
+        $social_profiles,
+        $source,
+        $stage,
+        $summary,
+        $tags,
+        $updated_date,
+        $work_history,
+        $custom_attributes
+    ) {
+        $this->_id = $_id;
+        $this->address = $address;
+        $this->assigned_to = $assigned_to;
+        $this->cover_letter = $cover_letter;
+        $this->creation_date = $creation_date;
+        $this->education = $education;
+        $this->email_address = $email_address;
+        $this->followed_by = $followed_by;
+        $this->headline = $headline;
+        $this->initial = $initial;
+        $this->name = $name;
+        $this->origin = $origin;
+        $this->overall_score = $overall_score;
+        $this->phone_number = $phone_number;
+        $this->profile_photo_url = $profile_photo_url;
+        $this->questionnaire = $questionnaire;
+        $this->recruited_by = $recruited_by;
+        $this->referred_by = $referred_by;
+        $this->sourced_by = $sourced_by;
+        $this->resume = $resume;
+        $this->social_profiles = $social_profiles;
+        $this->source = $source;
+        $this->stage = $stage;
+        $this->summary = $summary;
+        $this->tags = $tags;
+        $this->updated_date = $updated_date;
+        $this->work_history = $work_history;
+        $this->custom_attributes = $custom_attributes;
     }
 }
